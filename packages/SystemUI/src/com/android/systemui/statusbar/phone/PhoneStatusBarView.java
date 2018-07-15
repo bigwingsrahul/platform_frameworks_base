bc44b3e1aed2ad23d25a12c9955b030f01c98d52
Luca Stefani <luca.stefani.ge1@gmail.com>
2018-07-15 17:09:54 +0200

    SystemUI: Clock position customization
    Co-authored-by: Hendrik Hagendorn <git@finnq.de>
    Co-authored-by: kxxt <rsworktech@outlook.com>
    Co-authored-by: LuK1337 <priv.luk@gmail.com>
    Co-authored-by: Michael W <baddaemon87@gmail.com>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: Volodymyr Zhdanov <wight554@gmail.com>
    Co-authored-by: kxxt <rsworktech@outlook.com>
    Change-Id: Icdc039d13fd2676c1eb979a81fb77f9a6ec59b4a
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarView.java
@@ -24,8 +24,10 @@ import android.content.res.Resources;
 import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.Display;
 import android.view.DisplayCutout;
 import android.view.MotionEvent;
+import android.view.Surface;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowInsets;
@@ -347,6 +349,16 @@ public class PhoneStatusBarView extends FrameLayout {
                 insets.top,
                 insets.right,
                 getPaddingBottom());
+
+        // Apply negative paddings to centered area layout so that we'll actually be on the center.
+        Display display = getDisplay();
+        final int winRotation = display != null ? display.getRotation() : Surface.ROTATION_0;
+        LayoutParams centeredAreaParams =
+                (LayoutParams) findViewById(R.id.centered_area).getLayoutParams();
+        centeredAreaParams.leftMargin =
+                winRotation == Surface.ROTATION_0 ? -insets.left : 0;
+        centeredAreaParams.rightMargin =
+                winRotation == Surface.ROTATION_0 ? -insets.right : 0;
     }
 
     private void updateWindowHeight() {
