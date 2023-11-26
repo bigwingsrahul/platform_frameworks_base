44d91a51d9831cffcc2e419ef96fe3d9310edfc6
ezio84 <hrabus84@gmail.com>
2023-11-27 00:55:02 +0700

    frameworks: Add restart systemui shortcut to power menu [1/2]
    Google made it very easy on this cycle (systemui can just kill its own pid).
    
    Change-Id: I91021e906907737c47b09da57c0181ee23cbcde7
    [nurkeinneid] partial pick of DirtyUnicorns/android_frameworks_base@9c530663650a2af9534491d94c2c7d1f52344889
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialogLite.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialogLite.java
@@ -78,6 +78,7 @@ import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
 import android.os.PowerManager;
+import android.os.Process;
 import android.os.RemoteException;
 import android.os.SystemClock;
 import android.os.SystemProperties;
@@ -770,6 +771,8 @@ public class GlobalActionsDialogLite implements DialogInterface.OnDismissListene
                 addIfShouldShowAction(tempActions, new StandbyAction());
             } else if (GLOBAL_ACTION_KEY_DEVICECONTROLS.equals(actionKey)) {
                 addIfShouldShowAction(tempActions, new DeviceControlsAction());
+            } else if (GLOBAL_ACTION_KEY_RESTART_SYSTEMUI.equals(actionKey)) {
+                addIfShouldShowAction(tempActions, new RestartSystemUIAction());
             } else {
                 Log.e(TAG, "Invalid global action key " + actionKey);
             }
@@ -1531,6 +1534,27 @@ public class GlobalActionsDialogLite implements DialogInterface.OnDismissListene
         @Override
         public boolean showBeforeProvisioning() {
             return true;
+        }
+    }
+
+    private final class RestartSystemUIAction extends SinglePressAction {
+        private RestartSystemUIAction() {Add commentMore actions
+            super(com.android.systemui.R.drawable.ic_restart_systemui, com.android.systemui.R.string.global_action_restart_systemui);
+        }
+
+        @Override
+        public boolean showDuringKeyguard() {
+            return true;
+        }
+
+        @Override
+        public boolean showBeforeProvisioning() {
+            return true;
+        }
+
+        @Override
+        public void onPress() {
+            Process.killProcess(Process.myPid());
         }
     }
 
