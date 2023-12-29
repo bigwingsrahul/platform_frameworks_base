8a8f02389150cbbce17b285fcceaa47a735f1c46
Pranav Vashi <neobuddy89@gmail.com>
2023-12-29 23:11:59 +0530

    SystemUI: VolumeDialog: Dismiss dialog on config change
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -488,6 +488,9 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
 
     @Override
     public void onUiModeChanged() {
+        if (mDialog != null) {
+            mDialog.dismiss();
+        }
         mContext.getTheme().applyStyle(mContext.getThemeResId(), true);
         mConfigChanged = true;
     }
@@ -2986,7 +2989,11 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
 
     @Override
     public void onConfigChanged(Configuration config) {
+        if (mDialog != null) {
+            mDialog.dismiss();
+        }
         mOrientation = config.orientation;
+        mConfigChanged = true;
     }
 
     private final VolumeDialogController.Callbacks mControllerCallbackH
@@ -3017,7 +3024,9 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
 
         @Override
         public void onConfigurationChanged() {
-            mDialog.dismiss();
+            if (mDialog != null) {
+                mDialog.dismiss();
+            }
             mConfigChanged = true;
         }
 
