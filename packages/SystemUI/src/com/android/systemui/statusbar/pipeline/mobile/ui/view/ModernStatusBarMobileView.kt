AOSPA/android_frameworks_base-old

SystemUI: Fix suspicious spaces around new mobile icons
Referring to commit 7bb81fd27db87b00329b7cb3b7d95562700c4aa2, which addressed spacing issues around mobile icons. Reimplement this fix because this bug also appears in new mobile icons.

Change-Id: I34843940337d19fa729425b705d3901d2434f708
Signed-off-by: lgjint <lgjint@protonmail.com>

packages/SystemUI/src/com/android/systemui/statusbar/pipeline/mobile/ui/view/ModernStatusBarMobileView.kt
@@ -19,6 +19,7 @@ package com.android.systemui.statusbar.pipeline.mobile.ui.view
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.widget.LinearLayout
 import com.android.systemui.R
 import com.android.systemui.statusbar.StatusBarIconView.getVisibleStateString
 import com.android.systemui.statusbar.pipeline.mobile.ui.MobileViewLogger
@@ -32,6 +33,7 @@ class ModernStatusBarMobileView(
 ) : ModernStatusBarView(context, attrs) {
 
     var subId: Int = -1
+    private lateinit var mobileGroup: LinearLayout
 
     override fun toString(): String {
         return "ModernStatusBarMobileView(" +
@@ -40,6 +42,17 @@ class ModernStatusBarMobileView(
             "isCollecting=${binding.isCollecting()}, " +
             "visibleState=${getVisibleStateString(visibleState)}); " +
             "viewString=${super.toString()}"
+    }
+
+    override fun onFinishInflate() {
+        super.onFinishInflate()
+        mobileGroup = findViewById(R.id.mobile_group)
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        mobileGroup.measure(widthMeasureSpec, heightMeasureSpec)
+        setMeasuredDimension(mobileGroup.measuredWidth, mobileGroup.measuredHeight)
     }
 
     companion object {

bigwingsrahul/platform_frameworks_base

Co-authored-by: LibXZR <i@xzr.moe>

packages/SystemUI/src/com/android/systemui/statusbar/pipeline/mobile/ui/view/ModernStatusBarMobileView.kt
@@ -21,6 +21,7 @@ import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.FrameLayout
 import android.widget.ImageView
+import android.widget.LinearLayout
 import com.android.settingslib.flags.Flags.newStatusBarIcons
 import com.android.systemui.kairos.BuildSpec
 import com.android.systemui.kairos.ExperimentalKairosApi
@@ -42,6 +43,7 @@ class ModernStatusBarMobileView(context: Context, attrs: AttributeSet?) :
     ModernStatusBarView(context, attrs) {
 
     var subId: Int = -1
+    private lateinit var mobileGroup: LinearLayout
 
     override fun toString(): String {
         return "ModernStatusBarMobileView(" +
@@ -50,6 +52,17 @@ class ModernStatusBarMobileView(context: Context, attrs: AttributeSet?) :
             "isCollecting=${binding.isCollecting()}, " +
             "visibleState=${getVisibleStateString(visibleState)}); " +
             "viewString=${super.toString()}"
+    }
+
+    override fun onFinishInflate() {
+        super.onFinishInflate()
+        mobileGroup = requireViewById(R.id.mobile_group)
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        mobileGroup.measure(widthMeasureSpec, heightMeasureSpec)
+        setMeasuredDimension(mobileGroup.measuredWidth, mobileGroup.measuredHeight)
     }
 
     companion object {