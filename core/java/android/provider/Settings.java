928fd99ba8de04ba8b690eb0a151449f4842a7ba
Rahul <rahul731999@gmail.com>
2025-07-22 17:04:38 +0200

    Revert "base: support per-app volume [2/3]"
    This reverts commit 3b2b74f808948359cb6d26eb4e056a35e2905c64.
    
    Change-Id: I66614de69f2120b29018fc7c79785bb98422bac6

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6567,12 +6567,6 @@ public final class Settings {
         public static final String QS_BT_SHOW_DIALOG = "qs_bt_show_dialog";
 
         /**
-         * Show app volume rows in volume panel
-         * @hide
-         */
-        public static final String SHOW_APP_VOLUME = "show_app_volume";
-
-        /**
          * @hide
          */
         public static final String WIFI_STANDARD_ICON = "wifi_standard_icon";
@@ -21845,12 +21839,6 @@ public final class Settings {
         @SdkConstant(SdkConstant.SdkConstantType.ACTIVITY_INTENT_ACTION)
         public static final String ACTION_VOLUME =
                 "android.settings.panel.action.VOLUME";
-
-        /**
-         * @hide
-         */
-        public static final String ACTION_APP_VOLUME =
-                "android.settings.panel.action.APP_VOLUME";
     }
 
     /**
